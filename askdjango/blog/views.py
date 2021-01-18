2f45884d2121d042c882f40d5f79828f6db6b71e
eunhye <lime1pm@naver.com>
2021-01-19 01:12:45 +0900

    중급 02강

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+@login_required
+def post_new(request):
+    pass
